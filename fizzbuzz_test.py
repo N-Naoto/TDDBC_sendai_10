ae769b484bd94a5126fb3199db8a981e3768f139
N-Naoto <nao9909.n@gmail.com>
2021-03-03 21:24:07 +0900

    buzzのケースを追加

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -2,6 +2,9 @@ from fizzbuzz import fizzbuzz_convert
 
 
 class TestFizzBuzz:
-    def test_3で割り切れる整数をFizzに変換する(self):
+    def test_3で割り切れる整数をFizzに変換する_3の場合(self):
         assert fizzbuzz_convert(3) == 'Fizz'
+
+    def test_5で割り切れる整数をBuzzに変換する_5の場合(self):
+        assert fizzbuzz_convert(5) == 'Buzz'
 
