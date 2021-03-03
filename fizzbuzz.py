655213b3fb2af61731766f31529b345cd473f863
N-Naoto <nao9909.n@gmail.com>
2021-03-03 21:34:00 +0900

    3の倍数のテストケースを追加(6)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,5 @@
 def fizzbuzz_convert(num):
-    if num == 3:
+    if num % 3 == 0:
         return 'Fizz'
     if num == 5:
         return 'Buzz'
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,6 +5,9 @@ class TestFizzBuzz:
     def test_3で割り切れる整数をFizzに変換する_3の場合(self):
         assert fizzbuzz_convert(3) == 'Fizz'
 
+    def test_3で割り切れる整数をFizzに変換する_6の場合(self):
+        assert fizzbuzz_convert(6) == 'Fizz'
+
     def test_5で割り切れる整数をBuzzに変換する_5の場合(self):
         assert fizzbuzz_convert(5) == 'Buzz'
 
