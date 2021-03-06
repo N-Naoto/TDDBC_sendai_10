02a1939c8fc85afc791d3aecc0b3dd728f0655e2
mohira <shukatsudaison@gmail.com>
2021-03-06 12:45:38 +0900

    GREEN:テストケース１件追加

--- ichigo/ichigo_test.py
+++ ichigo/ichigo_test.py
@@ -11,3 +11,8 @@ class TestIchigo:
         ichigo = いちご("とちおとめ", "M")
 
         assert str(ichigo) == "とちおとめ: M"
+
+    def test_いちごの文字列表現を取得できる_もういっこLサイズ(self):
+        ichigo = いちご("もういっこ", "L")
+
+        assert str(ichigo) == "もういっこ: L"
