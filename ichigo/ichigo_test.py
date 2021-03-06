5fd7aa3ac9445b3a0cdcc0f9339a3440c31c9cdc
mohira <shukatsudaison@gmail.com>
2021-03-06 15:20:18 +0900

    MEMO: コードレビューをうけてTODOやMEMOを更新した

--- ichigo/ichigo_test.py
+++ ichigo/ichigo_test.py
@@ -23,6 +23,7 @@ class TestIchigo:
         assert str(ichigo) == "あまおう: LL"
 
     def test_サイズではなく重さを直接与えていちごを作成できる_20gはLサイズ(self):
+        # MEMO: メソッド名は and品種 も必要なのでは？
         ichigo = いちご.create_from_weight("あまおう", 20)
 
         assert str(ichigo) == "あまおう: L"
