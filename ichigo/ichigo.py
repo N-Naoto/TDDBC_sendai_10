0ec6eec0c272a0d420829d87d5bbd01a5bee213a
mohira <shukatsudaison@gmail.com>
2021-03-06 11:11:09 +0900

    RED: 文字列表現のメソッドを実装

--- ichigo/ichigo.py
+++ ichigo/ichigo.py
@@ -2,3 +2,7 @@ class Ichigo:
     def __init__(self, ichigo, size):
         self.ichigo = ichigo
         self.size = size
+
+    def __str__(self) -> str:
+        # str() を呼ぶと ここが実行される！ == 文字列表現
+        return "わーい！"
